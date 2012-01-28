dd54c5cdb1aeae4030989b6f1efc26af796e95d0
Sharad Vikram <sharad.vikram@gmail.com>
2012-01-27 21:15:10 -0800

    changed port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express.createServer();
 var dao = require('./dao.js');
+var port = 80;
 var user = function(email, token) {
   this.email = email;
   this.token = token;
@@ -12,4 +13,5 @@ app.use(express.static('../static'));
 app.get('/user.create', function(req, res) {
   newUser = new User(req.query['email'],req.query['token']);
 });
-app.listen(3000);
+app.listen(80);
+console.log("Server listening on port "+port);
