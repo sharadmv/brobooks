3c1dc283ef8bf100c07d0d63fef397e7f207ef52
Sharad Vikram <sharad.vikram@gmail.com>
2012-01-28 22:50:31 +0000

    added dao and api functionality

--- server/dao.js
+++ server/dao.js
@@ -1,17 +1,38 @@
-var client = new require('mysql').Client();
+var client = new require('mysql').createClient();
 Dao = function(host,port,user,password,database) {
   client.host = host;
   client.port = port;
   client.user = user;
   client.password = password;
   client.database = database;
-  client.query('select * from user',function selectCb(err,result,field){
-    console.log(err,result,field);
-  });
   this.User = {
-    createUser:function(user){
-       
-    }
+    create:function(user, callback){
+    	client.query("insert into user(email,token) values('"+user.email+"','"+user.token+"')", function selectCb(err,result,field){
+				if (!err) {
+					client.query("select * from user where email = '"+user.email+"' and token = '"+user.token+"'", function selectCb(err, result, field) {
+						callback(err, result, field);
+					});
+				} else {
+					callback(err, result);
+				}
+			});
+    },
+		get:function(user, callback) {
+			delimiter = " where ";
+			where = "";
+			for (var key in user){
+				where += delimiter + key+" = '"+user[key]+"'";
+				delimiter = " and ";
+			}
+			client.query("select * from user"+where, function selectCb(err,result,field){
+				callback(err, result);
+			});
+		},	
+		update:function(user, callback) {
+			client.query("update user set email='"+user.email+"' where user_id = "+user.user_id, function selectCb(err, result, field) {
+				callback(err, result);
+			});
+		}
   }
 };
-exports.Dao = Dao;
+exports.Dao = Dao
--- server/server.js
+++ server/server.js
@@ -1,11 +1,61 @@
 var express = require('express');
 var app = express.createServer();
 app.use(express.static('../static'));
-var dao = require('./dao.js')("localhost:3306","root");
-var port = 80;
+var Dao =  new require('./dao.js').Dao;
+var dao = new Dao("localhost",3306,"root","","brobooks");
+var port = 8080;
 app.use(express.static('../static'));
+app.enable("jsonp callback");
+//Classes
+var Error = function(code, message){
+	this.code = code;
+	this.message = message;
+}
+
 app.get('/user.create', function(req, res) {
-  newUser = new User(req.query['email'],req.query['token']);
+	console.log("user.create", req.params, req.query);
+	if (req.query['email'] && req.query['token']){
+		dao.User.create({email:req.query['email'],token:req.query['token']}, function(error, result) {
+			if (!error) {
+				res.json(result);
+			} else {
+				res.json(new Error(500, error.message));
+			}
+		});
+	} else {
+		res.json(new Error(400, "Parameters incorrect"));
+	}
+});
+app.get('/user.get', function(req, res) {
+	console.log("user.get", req.params, req.query);
+	if (req.query['user_id'] || req.query['email'] || req.query['token']){
+		dao.User.get(req.query, function(error, result) {
+			if (!error) {
+				res.json(result);	
+			} else {
+				res.json(new Error(500),error.message);
+			}
+		});
+	} else {
+		res.json(new Error(400, "Parameters incorrect"));
+	}
+});
+app.get('/user.update', function (req, res) {
+	console.log("user.update", req.params, req.query);
+	if (req.query['user_id'] && req.query['email']) {
+		dao.User.update(req.query, function(error, result) {
+			if (!error) {
+				res.json(result);
+			} else {
+				res.json(new Error(500, error.message));
+			}
+		});
+	} else {
+		res.json(new Error(400, "Parameters incorrect"));
+	}
+});
+app.get('/*', function (req, res) {
+	console.log(req.params, req.query);
 });
 app.listen(port);
 console.log("Server listening on port "+port);
