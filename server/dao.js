0794c2b414dba69ad7a4247c9ae395731fec5809
Sharad Vikram <sharad.vikram@gmail.com>
2012-03-24 17:49:37 -0700

    added preliminary dao methods

--- server/dao.js
+++ server/dao.js
@@ -1,7 +1,11 @@
 var db = require('mongoskin').db('localhost:27017/brobooks');
 var Dao = {
+  user:{
+    create:function(user, callback){
+    }
+  }
 }
-db.bind('user');
+//db.bind('user');
 db.user.find({fbId:1234}).toArray(function(err, items) {
   console.dir(items);
 });
