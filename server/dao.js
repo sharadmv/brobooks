8819ec31f24e82a8b2bb93ba3c2f602adaf21d5e
Sharad Vikram <sharad.vikram@gmail.com>
2012-03-24 18:05:49 -0700

    added user creation

--- server/dao.js
+++ server/dao.js
@@ -1,11 +1,13 @@
 var Message = require('./model.js').model.Message;
-var db = require('mongoskin').db('localhost:27017/brobooks');
+var db = require('mongoskin').db('107.108.20.117:27017/brobooks');
 db.bind('user');
 var Dao = {
   user:{
     create:function(user, callback){
-      db.user.save(user);
-      callback(
+      db.user.save(user,function(obj){
+        console.log(obj);
+      });
+      callback(new Message("success",200,null));
     }
   }
 }
