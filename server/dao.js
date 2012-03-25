a8840a18bde6c5efbb78a25df4440b85a553259f
Sharad Vikram <sharad.vikram@gmail.com>
2012-03-25 00:58:45 +0000

    aed slight db changes

--- server/dao.js
+++ server/dao.js
@@ -1,11 +1,14 @@
+var Message = require('./model.js').model.Message;
 var db = require('mongoskin').db('localhost:27017/brobooks');
+db.bind('user');
 var Dao = {
   user:{
     create:function(user, callback){
+      db.user.save(user);
+      callback(
     }
   }
 }
-//db.bind('user');
 db.user.find({fbId:1234}).toArray(function(err, items) {
   console.dir(items);
 });
