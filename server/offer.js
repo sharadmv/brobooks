559a8cf7ec82c248ea4465425f99460f881f067e
Sharad Vikram <sharad.vikram@gmail.com>
2012-03-25 22:37:58 -0700

    added debugging for offers

--- server/offer.js
+++ server/offer.js
@@ -2,6 +2,7 @@ var Offer = require('./model.js').Offer;
 var offer = function(dao){
   this.dao = dao;
   this.create = function(obj, callback) {
+    console.log(dao);
     offer = new Offer(obj.user, obj.book, obj.price, obj.condition, obj.time, obj.loc, 1);  
     dao.offer.update(offer, callback);
   }
