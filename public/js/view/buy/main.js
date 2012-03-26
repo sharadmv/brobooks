f27282a6840c6cfb1624f8260b78e59a7b6c0428
Sharad Vikram <sharad.vikram@gmail.com>
2012-03-26 16:57:05 -0700

    added autocomplete for buy books

--- public/js/view/buy/main.js
+++ public/js/view/buy/main.js
@@ -1,16 +1,39 @@
 define([
-  'jQuery',
-  'Underscore',
-  'Backbone',
-  'text!template/buy/main.html'
+    'jQuery',
+    'Underscore',
+    'Backbone',
+    'text!template/buy/main.html'
 ], function($, _, Backbone, buyMainTemplate) {
   var BuyMainView = Backbone.View.extend({
-    el: $('#content'),
-    render: function() {
-      var data = {};
-      var compiledTemplate = _.template(buyMainTemplate, data);
-      this.$el.html(compiledTemplate);
-    }
+      el: $('#content'),
+      render: function() {
+        var data = {};
+        var compiledTemplate = _.template(buyMainTemplate, data);
+        this.$el.html(compiledTemplate);
+        $( "#buy-lecture-select" ).autocomplete({
+            source: function( request, response ) {
+              $.ajax({
+                  url: "/api/service",
+                  dataType: "jsonp",
+                  data: {
+                    name:'scraper.catalog',
+                    params:{
+                      name:request.term 
+                    }
+                  },
+                  success: function(message) {
+                    if (message.code == 200) {
+                      response($.map(message.result, function(item) {
+                        return item;
+                        })
+                      );
+                    }
+                  }
+              });
+            },
+            minLength: 2,
+        });
+      }
   });
   return new BuyMainView;
 });
