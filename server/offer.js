8b0d82fd15bb173158cf18e69c3907e52802b627
Sharad Vikram <sharad.vikram@gmail.com>
2012-03-25 22:34:16 -0700

    changed offer create params

--- server/offer.js
+++ server/offer.js
@@ -1,8 +1,8 @@
 var Offer = require('./model.js').Offer;
 var offer = function(dao){
   this.dao = dao;
-  this.create = function(user, book, price, time, loc, condition, callback) {
-    offer = new Offer(user, book, price, condition, time, loc, 1);  
+  this.create = function(obj, callback) {
+    offer = new Offer(obj.user, obj.book, obj.price, obj.condition, obj.time, obj.loc, 1);  
     dao.offer.update(offer, callback);
   }
 }
