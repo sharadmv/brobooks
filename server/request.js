779d82d7d71e248d5eaff2b3a08a6264ec1a2e32
Sharad Vikram <sharad.vikram@gmail.com>
2012-03-26 11:37:07 -0700

    added FB sorting by mutual friends

--- server/request.js
+++ server/request.js
@@ -1,11 +1,24 @@
 var Request = require('./model.js').model.Request;
+var FB = require('./fb.js').FB;
 var request = function(dao){
   this.dao = dao;
   this.create = function(obj, callback) {
     request = new Request(obj.user, obj.book, 1); 
     dao.request.update(request, function(msg){
       dao.offer.find({book:{isbn:request.book.isbn}}, function(message) {
-        callback(message.result);
+        var count = 0;
+        for (var i = 0;i<message.result.length;i++){
+          FB.mutual(obj.user, message.result[i].user, function(mutual){
+            message.result[i].mutual = mutual.length;
+            count++;
+            if (count == message.result.length){
+              message.result.sort(function(a,b){
+                return a.mutual-b.mutual;
+              });
+              callback(message.result);
+            }
+          });
+        }
       });
     });
   }
