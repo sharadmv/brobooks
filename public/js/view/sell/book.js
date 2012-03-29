dea7189cf0b99a19e81a1a0b83411561b7a603f0
Sharad Vikram <sharad.vikram@gmail.com>
2012-03-29 02:56:10 +0000

    added sell views

---
+++ public/js/view/sell/book.js
@@ -0,0 +1,26 @@
+define([
+    'jQuery',
+    'Underscore',
+    'Backbone',
+    'text!template/buy/book.html',
+    'app'
+], function($, _, Backbone, buyBookTemplate, app) {
+  var BuyBookView = Backbone.View.extend({
+    tagName:'option',
+    template:_.template(buyBookTemplate),
+    render: function() {
+      var that = this;
+      $("#buy-book").change( function(e) {
+        if( e.target.value == that.model.attributes.title) {
+          that.getBook();
+        }
+      });
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    },
+    getBook:function(){
+      /*app.selectBook(this.model);*/
+    }
+  });
+  return BuyBookView;
+});
